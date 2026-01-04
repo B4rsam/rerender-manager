bf736b9ab92ac894abd0b68fa1cd27e6d5ba40ae
Barsam Badakhshani <105477761+B4rsam@users.noreply.github.com>
2026-01-04 16:32:42 +0330

    Added item deletion

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 let list = [];
 
+const handleDelete = (itemID) => {
+    list = list.filter(({ id }) => itemID !== id);
+    generateList();
+};
+
 const generateList = () => {
     const container = document.getElementById("list-container");
+    let data = "";
 
     list.forEach((item) => {
-        const child = `
+        const child = `\n
             <div class="card" id="quote-${item.id}">
-                <h2>${item.quote}</h2>
-                <p>${item.author}</p>
+                <section class="text">
+                    <h2>${item.quote}</h2>
+                    <p>${item.author}</p>
+                </section>
+                <section class="btns">
+                    <button class="delete-button" id="delete-${item.id}">Delete</button>
+                </section>
             </div>
         `;
 
-        container.innerHTML += child;        
+        data += child;
+
     });
 
+    container.innerHTML = data;
+
+    generateEventListeners();
+
     return;
+};
+
+const generateEventListeners = () => {
+    list.forEach(({ id }) => {
+        document.getElementById("delete-" + id).addEventListener("click", () => handleDelete(id));
+    });
 };
 
 const getData = () => {
     fetch("https://dummyjson.com/quotes").then((res) => res.json())
     .then((data) => {
         list = data.quotes;
-        console.log(data)
         generateList();
     })
     .catch((err) => {
