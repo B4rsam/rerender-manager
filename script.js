79c3893191c248f19335095b5315e1708564a7ef
Barsam Badakhshani <105477761+B4rsam@users.noreply.github.com>
2026-01-04 16:21:00 +0330

    Added list

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("hi")
+let list = [];
+
+const generateList = () => {
+    const container = document.getElementById("list-container");
+
+    list.forEach((item) => {
+        const child = `
+            <div class="card" id="quote-${item.id}">
+                <h2>${item.quote}</h2>
+                <p>${item.author}</p>
+            </div>
+        `;
+
+        container.innerHTML += child;        
+    });
+
+    return;
+};
+
+const getData = () => {
+    fetch("https://dummyjson.com/quotes").then((res) => res.json())
+    .then((data) => {
+        list = data.quotes;
+        console.log(data)
+        generateList();
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
+};
+
+getData();
